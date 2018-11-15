a78b77c60d311d15161c38f0d0180fe3e0c7f1ea
bigfoolliu <liu15671677014@icloud.com>
2018-11-15 17:51:08 +0800

    贴吧图片爬取xpath实现

---
+++ day3/tieba_image.py
@@ -0,0 +1,142 @@
+#!-*-coding:utf-8-*-
+# !@Date: 2018/11/14 21:50
+# !@Author: Liu Rui
+# !@github: bigfoolliu
+
+
+"""
+爬取贴吧图片
+
+1. 输入百度贴吧名
+2. 按下enter键开始爬取第一页的所有帖子里的所有发布的图片
+3. 按下q或者Q结束爬取
+
+贴吧网址: https://tieba.baidu.com/
+键入贴吧名搜索网址: https://tieba.baidu.com/f?ie=utf-8&kw=%E6%BC%AB%E5%A8%81&fr=search
+
+第一页网址: https://tieba.baidu.com/f?kw=%E6%BC%AB%E5%A8%81&ie=utf-8&pn=0
+第二页网址: https://tieba.baidu.com/f?kw=%E6%BC%AB%E5%A8%81&ie=utf-8&pn=50
+...
+
+
+
+每页中所有的帖子的标题跳转标签:
+<a rel="noreferrer" href="/p/5950220601" title="狼叔最近是要逆天的节奏啊" target="_blank" class="j_th_tit ">
+狼叔最近是要逆天的节奏啊</a>
+
+获取到href的值然后重新发请求进入到具体的帖子
+
+
+
+具体的一个帖子网址:
+https://tieba.baidu.com/p/5950316508
+
+具体帖子中发表的图片的标签:
+<img class="BDE_Image" src="https://imgsa.baidu.com/forum/w%3D580/sign=61b03229a0345982c58ae59a3cf5310b/
+d17eadfb43166d22b927b3c94b2309f79152d2a5image.jpg" size="107599" changedsize="true" width="560" height="840">
+
+需要获取图片的src并将图片下载保存
+"""
+from lxml import etree
+import requests
+
+
+class TiebaImageSpider(object):
+    """
+    贴吧图片爬虫
+    """
+    def __init__(self):
+        self.base_url = 'https://tieba.baidu.com/f?'
+        self.page_base_url = 'https://tieba.baidu.com'
+        self.page = 1
+        self.query_dict = {
+            'ie': 'utf-8',
+            'kw': input('请输入贴吧名:'),
+            'pn': str((self.page - 1) * 50)
+        }
+        # TODO: Chrome浏览器对百度贴吧有优化,所有的帖子都在注释里,需要用ie的请求头才能用 xpath 提取
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+
+    def send_request(self, url, query_dict=None, headers=None):
+        """
+        发送请求,返回响应
+        :param url:
+        :param query_dict:
+        :param headers:
+        :return:
+        """
+        print('[INFO]: 开始发送响应...')
+        response = requests.get(url, params=query_dict, headers=headers)
+        return response
+
+    def parse_page(self, response):
+        """
+        解析当前页面,返回所有的帖子列表
+        :param response:
+        :return:
+        """
+        print('[INFO]: 开始解析页面...')
+
+        html = response.content.decode('utf-8')
+        html_obj = etree.HTML(html)
+        page_link_list = html_obj.xpath("//a[@class='j_th_tit ']/@href")
+
+        return page_link_list
+
+    def parse_image(self, response):
+        """
+        解析帖子,获取到所有的图片地址列表
+        :param response:
+        :return:
+        """
+        print('[INFO]: 开始解析帖子中的图片链接...')
+
+        html = response.content.decode('utf-8')
+        html_obj = etree.HTML(html)
+        image_url_list = html_obj.xpath("//img[@class='BDE_Image']/@src")
+
+        return image_url_list
+
+    def save_image(self, image_response, file_name):
+        """
+        保存图片
+        :param image_response:
+        :param file_name:
+        :return:
+        """
+        image_data = image_response.content
+
+        print('[INFO]: 正在保存文件 {}'.format(file_name))
+        with open(file_name, 'wb') as f:
+            f.write(image_data)
+
+    def run(self):
+        while True:
+            if input('输入(q结束):').lower() == 'q':
+                break
+
+            try:
+                response = self.send_request(self.base_url, query_dict=self.query_dict, headers=self.headers)
+                print('[INFO]: 响应成功...')
+
+                page_link_list = self.parse_page(response)
+
+                for page in page_link_list:
+                    # 重新构造请求url
+                    page_url = self.page_base_url + page
+                    page_response = self.send_request(page_url)
+                    image_url_list = self.parse_image(page_response)
+
+                    for image_url in image_url_list:
+                        image_response = self.send_request(image_url)
+                        # TODO: 取图片url的最后十位数字作为图片的名称
+                        file_name = 'images/' + image_url[-14:]
+                        self.save_image(image_response, file_name=file_name)
+
+            except Exception as e:
+                print('[INFO]: 产生异常:{}'.format(e))
+
+
+if __name__ == '__main__':
+    TiebaImageSpider().run()
+
